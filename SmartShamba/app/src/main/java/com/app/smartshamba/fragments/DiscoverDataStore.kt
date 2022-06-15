669189b59e8c7bb6f5934538475936d3a47adf4f
William <bobliam20gitlm@gmail.com>
2022-06-15 07:41:24 +0300

    added Discover data Store

---
+++ SmartShamba/app/src/main/java/com/app/smartshamba/fragments/DiscoverDataStore.kt
@@ -0,0 +1,24 @@
+package com.app.smartshamba.fragments
+
+class DiscoverDataStore {
+    lateinit var overview:Array<String>
+    var preparations:Array<String>? = null
+    var pestControl:Array<String>? = null
+    var harvesting:Array<String>? = null
+    var conclusion:Array<String>? = null
+    var title7:Array<String>? = null
+    var descOverview:Array<String>? = null
+    var descPreparation:Array<String>? = null
+    var descPestControl:Array<String>? = null
+    var descHarvesting:Array<String>? = null
+    var desc5Conclusion:Array<String>? = null
+    var desc6:Array<String>? = null
+    var desc7:Array<String>? = null
+
+    fun dataStore(){
+        overview = arrayOf(
+            "Overview", "Preparation", "Disease Control", "Harvesting", "Conclusion"
+        )
+
+    }
+}
